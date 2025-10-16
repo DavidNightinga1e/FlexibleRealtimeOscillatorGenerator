4b669ccce1a1b825e830b1668420a5729a52ff7d
David Nightingale <nightrider2552@gmail.com>
2025-10-17 01:32:41 +0300

    some ui

---
+++ Assets/Runtime/UI/IntegerKnob.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Runtime.UI
+{
+	public class IntegerKnob : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+	{
+		[SerializeField] private List<float> rotations;
+		[SerializeField] private float rotationMultiplier;
+		[SerializeField] private RectTransform knob;
+
+		private Vector2 _dragStartPosition;
+		private float _currentRotation;
+		
+		public int Value { get; private set; }
+
+		private void Awake()
+		{
+			SyncViewToState();
+		}
+
+		public void OnBeginDrag(PointerEventData eventData)
+		{
+			_dragStartPosition = eventData.position;
+			_currentRotation = knob.rotation.eulerAngles.z;
+		}
+
+		public void OnEndDrag(PointerEventData eventData)
+		{
+			int closestRotationIndex = 0;
+			float closestDistance = float.MaxValue;
+			for (int i = 0; i < rotations.Count; i++)
+			{
+				float distance = Mathf.Abs(knob.eulerAngles.z - rotations[i]);
+				if (distance < closestDistance)
+				{
+					closestRotationIndex = i;
+					closestDistance = distance;
+				}
+			}
+			
+			Value = closestRotationIndex;
+			
+			SyncViewToState();
+		}
+
+		public void OnDrag(PointerEventData eventData)
+		{
+			Vector2 delta = eventData.position - _dragStartPosition;
+			var rotation = delta.x * rotationMultiplier + delta.y * rotationMultiplier;
+			knob.eulerAngles = new Vector3(0, 0, _currentRotation + rotation);
+		}
+
+		private void SyncViewToState()
+		{
+			knob.eulerAngles = new Vector3(0, 0, rotations[Value]);
+		}
+	}
+}
---
+++ Assets/Runtime/UI/Toggle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Runtime.UI
+{
+	public class Toggle : MonoBehaviour
+	{
+		[SerializeField] private Button button;
+		[SerializeField] private Image on;
+		[SerializeField] private Image off;
+		
+		public bool State { get; private set; }
+		public event Action<bool> OnStateChanged; 
+
+		private void Awake()
+		{
+			button.onClick.AddListener(OnButtonClick);
+			SyncViewToState();
+		}
+
+		private void SyncViewToState()
+		{
+			on.enabled = State;
+			off.enabled = !State;
+		}
+
+		private void OnButtonClick()
+		{
+			State = !State;
+			SyncViewToState();
+			OnStateChanged?.Invoke(State);
+		}
+	}
+}
