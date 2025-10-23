641cd2f3f16e1cc138c5587d7a77b695e5b888a4
David Nightingale <nightrider2552@gmail.com>
2025-10-24 00:49:02 +0300

    synth preset cs

---
+++ Assets/Runtime/Synth/Settings/SynthesizerPreset.cs
@@ -0,0 +1,17 @@
+﻿namespace Runtime.Synth
+{
+	public class SynthesizerPreset
+	{
+		public OscillatorSettings Osc1Settings { get; set; }
+		public OscillatorSettings Osc2Settings { get; set; }
+		public LfoSettings Lfo1Settings { get; set; }
+		public LfoSettings Lfo2Settings { get; set; }
+		public FilterSettings FilterSettings { get; set; }
+		public EnvelopeSettings AmpSettings { get; set; }
+		public EnvelopeSettings Env1Settings { get; set; }
+		public EnvelopeSettings Env2Settings { get; set; }
+		public DistortSettings DistortSettings { get; set; }
+		public DelaySettings DelaySettings { get; set; }
+		public ReverbSettings ReverbSettings { get; set; }
+	}
+}
